ff0d006319f060588a2b34c61f625651c6d035b0
Matus Goljer <dota.keys@gmail.com>
2014-07-22 16:37:52 +0200

    Add ido buffer-filtering function

--- files/ido-defs.el
+++ files/ido-defs.el
@@ -4,6 +4,15 @@
 (defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
 (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
 
+(defun my-ido-buffer-filter (buffer)
+  "Should we try to switch to BUFFER?"
+  (let ((frame-type (frame-parameter (selected-frame) :frame-type)))
+    (cond
+     ((eq frame-type :circe)
+      (with-current-buffer buffer
+        (not (memq major-mode '(circe-channel-mode circe-server-mode)))))
+     (t nil))))
+
 ;; see: http://emacswiki.org/emacs/InteractivelyDoThings#toc25
 (defun ido-smart-select-text ()
   "Select the current completed item.  Do NOT descend into directories."
