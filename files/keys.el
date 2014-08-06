4c789f89830fc471b6cca449bca39d553579e1ad
Matus Goljer <dota.keys@gmail.com>
2014-08-06 20:01:12 +0200

    Add more input methods

--- files/keys.el
+++ files/keys.el
@@ -251,7 +251,9 @@
   ("t" . (lambda () "Toggle on TeX input method." (interactive) (set-input-method "TeX")))
   ("l" . (lambda () "Toggle on latin-macrons input method." (interactive) (set-input-method "latin-macrons")))
   ("f" . (lambda () "Toggle on french-keyboard input method." (interactive) (set-input-method "french-keyboard")))
-  ("g" . (lambda () "Toggle on greek-mizuochi input method." (interactive) (set-input-method "greek-mizuochi"))))
+  ("g" . (lambda () "Toggle on greek-mizuochi input method." (interactive) (set-input-method "greek-mizuochi")))
+  ("j" . (lambda () "Toggle on japanese input method." (interactive) (set-input-method "japanese")))
+  ("x" . (lambda () "Toggle on ipa-x-sampa input method." (interactive) (set-input-method "ipa-x-sampa"))))
 
 (bind-key "H-u" 'universal-argument)
 (bind-key "H-u" 'universal-argument-more universal-argument-map)
