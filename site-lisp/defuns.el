f0f3ce0802d0d489d48c48bb760d3704fa19f4a8
Matus Goljer <dota.keys@gmail.com>
2014-08-31 12:31:18 +0200

    Add some code to work with text-properties (should get merged into s.el)

--- site-lisp/defuns.el
+++ site-lisp/defuns.el
@@ -311,6 +311,52 @@ The point is moved to the end of the match."
                   (backward-char)
                   (insert ", " plural))))))))))
 
+;; should go into s.el
+(defun my-next-property-value (start prop val &optional object limit)
+  "Find next position where PROP has value VAL."
+  (let ((point start) done)
+    (or (and (equal (plist-get (text-properties-at point object) prop) val)
+             point)
+        (progn
+          (while (and (not done)
+                      (if (or (not object)
+                              (bufferp object))
+                          (/= point (point-max))
+                        (/= point (1- (length object))))
+                      (setq point (next-single-property-change point prop object limit)))
+            (setq done (equal (plist-get (text-properties-at point object) prop) val)))
+          point))))
+
+;; should go into s.el
+(defun my-next-property-not-value (start prop val &optional object limit)
+  "Find next position where PROP does not have value VAL."
+  (let ((point start) done)
+    (or (and (not (equal (plist-get (text-properties-at point object) prop) val))
+             point)
+        (progn
+          (while (and (not done)
+                      (if (or (not object)
+                              (bufferp object))
+                          (/= point (point-max))
+                        (/= point (1- (length object))))
+                      (setq point (next-single-property-change point prop object limit)))
+            (setq done (not (equal (plist-get (text-properties-at point object) prop) val))))
+          point))))
+
+;; ported from gnus-*... should go into s.el
+;; TODO: add more general version where we can pass a predicate
+(defun my-remove-text-properties-when
+  (property value start end properties &optional object)
+  "Like `remove-text-properties', only applied on where PROPERTY is VALUE."
+  (let (point)
+    (setq start (my-next-property-value start property value object end))
+    (while (and start
+                (< start end)
+                (setq point (my-next-property-not-value start property value object end)))
+      (remove-text-properties start point properties object)
+      (setq start (my-next-property-value point property value object end)))
+    object))
+
 (defun my-sprunge (text &optional language)
   "Paste TEXT to sprunge.us.
 
