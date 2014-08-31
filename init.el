79cf291c5532ba36b6bc0ee7ea31f28ab6a61584
Matus Goljer <dota.keys@gmail.com>
2014-08-31 12:29:52 +0200

    Disable marmalade for now

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 
 ;; add repos
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
