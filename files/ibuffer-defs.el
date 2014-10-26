c205d7527ce140f4761fe5b687acc68be73362b0
Matus Goljer <dota.keys@gmail.com>
2014-10-26 16:58:31 +0100

    [ibuffer] setq-default ibuffer-saved-filter-groups

--- files/ibuffer-defs.el
+++ files/ibuffer-defs.el
@@ -6,80 +6,81 @@
            ("/ y" . ibuffer-filter-by-default-directory))
 
 ;; filter groups
-(setq ibuffer-saved-filter-groups
-      (quote (("default"
-               ("Org" ;; all org-related buffers
-                (mode . org-mode))
-               ("emacs-elpa"
-                (or (predicate
-                     .
-                     (let ((bfn (buffer-file-name (current-buffer))))
-                       (when bfn
-                         (and (string-match-p "\\.emacs\\.d/elpa" bfn)
-                              (eq major-mode 'emacs-lisp-mode)))))))
-               ("emacs-config"
-                (or (predicate
-                     .
-                     (let ((bfn (buffer-file-name (current-buffer))))
-                       (when bfn
-                         (and (string-match-p "\\.emacs\\.d" bfn)
-                              (eq major-mode 'emacs-lisp-mode)))))))
-               ("emacs"
-                (or (mode . emacs-lisp-mode)
-                    (mode . lisp-interaction-mode)
-                    (mode . inferior-emacs-lisp-mode)))
-               ("TeX"
-                (or (mode . tex-mode)
-                    (mode . plain-tex-mode)
-                    (mode . latex-mode)
-                    (mode . bibtex-mode)))
-               ("Markdown" (or (mode . markdown-mode)
-                               (mode . gfm-mode)))
-               ("Web"
-                (or (mode . html-mode)
-                    (mode . css-mode)
-                    (mode . php-mode)
-                    (mode . js-mode)))
-               ("Dired"
-                (mode . dired-mode))
-               ("Langs"
-                (or (predicate
-                     .
-                     (let ((bfn (buffer-file-name (current-buffer))))
-                       (when bfn
-                         (string-match-p "d:/languages" bfn))))))
-               ("Images"
-                (or (mode . image-dired-display-image-mode)
-                    (mode . image-dired-thumbnail-mode)
-                    (mode . image-mode)))
-               ("Tramp"
-                (or (name . "tramp")))
-               ("Programming" ;; prog stuff not already in MyProjectX
-                (or
-                 (mode . c-mode)
-                 (mode . cc-mode)
-                 (mode . c++-mode)
-                 (mode . js3-mode)
-                 (mode . perl-mode)
-                 (mode . python-mode)
-                 (mode . haskell-mode)
-                 (mode . malabar-mode)
-                 (mode . makefile-gmake-mode)
-                 ;; etc
-                 ))
-               ("IRC"
-                (or (mode . erc-mode)
-                    (mode . rcirc-mode)
-                    (mode . circe-channel-mode)
-                    (mode . circe-server-mode)))
-               ("Search"
-                (or (mode . ag-mode)))
-               ("Org Agenda"
-                (or (mode . org-agenda-mode)))
-               ("Images"
-                (or (mode . di-view-mode)
-                    (mode . di-thumb-mode)))
-               ))))
+(setq-default
+ ibuffer-saved-filter-groups
+ (quote (("default"
+          ("Org" ;; all org-related buffers
+           (mode . org-mode))
+          ("emacs-elpa"
+           (or (predicate
+                .
+                (let ((bfn (buffer-file-name (current-buffer))))
+                  (when bfn
+                    (and (string-match-p "\\.emacs\\.d/elpa" bfn)
+                         (eq major-mode 'emacs-lisp-mode)))))))
+          ("emacs-config"
+           (or (predicate
+                .
+                (let ((bfn (buffer-file-name (current-buffer))))
+                  (when bfn
+                    (and (string-match-p "\\.emacs\\.d" bfn)
+                         (eq major-mode 'emacs-lisp-mode)))))))
+          ("emacs"
+           (or (mode . emacs-lisp-mode)
+               (mode . lisp-interaction-mode)
+               (mode . inferior-emacs-lisp-mode)))
+          ("TeX"
+           (or (mode . tex-mode)
+               (mode . plain-tex-mode)
+               (mode . latex-mode)
+               (mode . bibtex-mode)))
+          ("Markdown" (or (mode . markdown-mode)
+                          (mode . gfm-mode)))
+          ("Web"
+           (or (mode . html-mode)
+               (mode . css-mode)
+               (mode . php-mode)
+               (mode . js-mode)))
+          ("Dired"
+           (mode . dired-mode))
+          ("Langs"
+           (or (predicate
+                .
+                (let ((bfn (buffer-file-name (current-buffer))))
+                  (when bfn
+                    (string-match-p "d:/languages" bfn))))))
+          ("Images"
+           (or (mode . image-dired-display-image-mode)
+               (mode . image-dired-thumbnail-mode)
+               (mode . image-mode)))
+          ("Tramp"
+           (or (name . "tramp")))
+          ("Programming" ;; prog stuff not already in MyProjectX
+           (or
+            (mode . c-mode)
+            (mode . cc-mode)
+            (mode . c++-mode)
+            (mode . js3-mode)
+            (mode . perl-mode)
+            (mode . python-mode)
+            (mode . haskell-mode)
+            (mode . malabar-mode)
+            (mode . makefile-gmake-mode)
+            ;; etc
+            ))
+          ("IRC"
+           (or (mode . erc-mode)
+               (mode . rcirc-mode)
+               (mode . circe-channel-mode)
+               (mode . circe-server-mode)))
+          ("Search"
+           (or (mode . ag-mode)))
+          ("Org Agenda"
+           (or (mode . org-agenda-mode)))
+          ("Images"
+           (or (mode . di-view-mode)
+               (mode . di-thumb-mode)))
+          ))))
 
 (define-ibuffer-filter default-directory
     "Toggle current view to buffers whose default-directory matches QUALIFIER."
