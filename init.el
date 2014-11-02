0786ec5450187086f6097cff8eaec78644ce9a81
Matus Goljer <dota.keys@gmail.com>
2014-11-02 10:08:49 +0100

    Update melpa, readd marmalade

--- init.el
+++ init.el
@@ -13,8 +13,8 @@
 
 ;; add repos
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
 (use-package-with-elapsed-timer "Initializing packages"
