1163600ff8a998923638c125653a759d71546247
Matus Goljer <dota.keys@gmail.com>
2015-02-12 23:44:31 +0100

    Add ox-latex settings xetex support

--- files/org-defs.el
+++ files/org-defs.el
@@ -458,6 +458,78 @@ replace any running timer."
                 (org-timer-set-mode-line 'on))
             (message "No timer set")))))))
 
+(use-package ox-latex
+  :defer t
+  :config
+  (setq org-latex-listings t)
+
+  (defadvice org-latex-export-to-latex (before set-environment activate)
+    (my-auto-tex-parameters)
+    (my-auto-tex-cmd))
+
+  ;; Originally taken from Bruno Tavernier: http://thread.gmane.org/gmane.emacs.orgmode/31150/focus=31432
+  ;; but adapted to use latexmk 4.20 or higher.
+  (defun my-auto-tex-cmd ()
+    "When exporting from .org with latex, automatically run latex,
+     pdflatex, or xelatex as appropriate, using latexmk."
+    (let (texcmd)
+      ;; default command: oldstyle latex via dvi
+      (setq texcmd "latexmk -dvi -pdfps -quiet %f")
+      ;; pdflatex -> .pdf
+      (if (string-match "LATEX_CMD: pdflatex" (buffer-string))
+          (setq texcmd "latexmk -pdf -quiet %f"))
+      ;; xelatex -> .pdf
+      (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+          (setq texcmd "latexmk -pdflatex=xelatex -pdf -quiet %f"))
+      ;; LaTeX compilation command
+      (setq org-latex-to-pdf-process (list texcmd))))
+
+  ;; Specify default packages to be included in every tex file, whether pdflatex or xelatex
+  (setq org-latex-packages-alist
+        '(("" "graphicx" t)
+          ("" "longtable" nil)
+          ("" "float" nil)))
+
+  (defun my-auto-tex-parameters ()
+    "Automatically select the tex packages to include."
+    ;; default packages for ordinary latex or pdflatex export
+    (setq org-latex-default-packages-alist
+          '(("AUTO" "inputenc" t)
+            ("T1"   "fontenc"   t)
+            (""     "fixltx2e"  nil)
+            (""     "wrapfig"   nil)
+            (""     "soul"      t)
+            (""     "textcomp"  t)
+            (""     "marvosym"  t)
+            (""     "wasysym"   t)
+            (""     "latexsym"  t)
+            (""     "amssymb"   t)
+            (""     "hyperref"  nil)))
+
+    ;; Packages to include when xelatex is used
+    (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+        (setq org-latex-default-packages-alist
+              '(("" "fontspec" t)
+                ("" "xunicode" t)
+                ("" "url" t)
+                ("" "rotating" t)
+                ("" "babel" t)
+                ("babel" "csquotes" t)
+                ("" "soul" t)
+                ("xetex" "hyperref" nil)
+                )))
+
+    (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+        (setq org-latex-classes
+              (cons '("article"
+                      "\\documentclass[11pt,article,oneside]{memoir}"
+                      ("\\section{%s}" . "\\section*{%s}")
+                      ("\\subsection{%s}" . "\\subsection*{%s}")
+                      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                      ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                      ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+                    org-latex-classes)))))
+
 ;; add support for automatic org-files commits
 (defvar my-org-commit-timer
   (run-at-time (format-time-string "%H:59" (current-time)) 3600 'org-save-all-org-buffers)
