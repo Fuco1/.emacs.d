03c48f7e2a6322ae16983b54632bfff120929d84
Matus Goljer <dota.keys@gmail.com>
2015-02-14 21:53:23 +0100

    Remove unused macros

--- site-lisp/defuns-macros.el
+++ site-lisp/defuns-macros.el
@@ -1,50 +1,3 @@
-(defmacro save-kill-ring (&rest body)
-  "Save the kill ring and execute the BODY.  After BODY is
-executed, the kill-ring value is restored to the state before
-BODY was executed"
-  (declare (indent 0))
-  ;; initialize new binding for the ,@body.  It starts with the global
-  ;; original value.  After ,@body is executed, the new binding is
-  ;; thrown away!
-  `(let ((kill-ring kill-ring))
-     ,@body))
-
-(defmacro my-save-buffer-list (&rest forms)
-  "Execute FORMS while preserving the list of opened buffers.
-After the FORMS are executed, close all buffers that were not
-opened before.
-
-Note that this macro does not reopen buffers closed during the
-execution of forms."
-  (declare (indent 0))
-  `(let ((old-buffer-list (buffer-list)))
-     ,@forms
-     (mapc (lambda (buffer)
-             (unless (member buffer old-buffer-list)
-               (kill-buffer buffer)))
-           (buffer-list))))
-
-;; this is still broken :/
-(defmacro with-files-in-dir (directory &rest forms)
-  (declare (indent 1))
-  `(my-save-buffer-list
-     (save-excursion
-       (mapc (lambda (file)
-               (find-file (concat ,directory "/" file))
-               ,@forms)
-             (directory-files ,directory)))))
-
-(defmacro with-lines-narrow (&rest forms)
-  (declare (indent 0))
-  `(while (not (eobp))
-     (let ((beg (point-at-bol))
-           (end (point-at-eol)))
-       (save-excursion
-         (save-restriction
-           (narrow-to-region beg end)
-           ,@forms)))
-     (forward-line)))
-
 (defmacro with-every-line (&rest forms)
   "Execute FORMS on each line following point to the end of file."
   (declare (indent 0))
@@ -61,11 +14,6 @@ execution of forms."
 
 (defvar my-macro-names
   '(
-    "my-save-buffer-list"
-    "save-kill-ring"
-    "with-files-in-dir"
-    "with-lines-narrow"
-    "with-every-line"
     "fix-reset-after-each"
     "bind-keys"
     ))
