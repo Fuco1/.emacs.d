d741f94d9f5ca2fd9740d4bcbecb835f6515c1c8
Matus Goljer <dota.keys@gmail.com>
2015-02-14 21:40:36 +0100

    Better before-kill advice

--- site-lisp/advices.el
+++ site-lisp/advices.el
@@ -1,35 +1,15 @@
 ;; from simple.el
 (defadvice kill-line (before kill-line-autoreindent activate)
-  (if (member major-mode
-              '(
-                lisp-interaction-mode
-                emacs-lisp-mode
-                scheme-mode
-                lisp-mode
-                c-mode
-                c++-mode
-                latex-mode
-                ))
-      (if (and (eolp) (not (bolp)))
-          (progn (forward-char 1)
-                 (just-one-space 0)
-                 (backward-char 1)))))
+  (when (and (eolp) (not (bolp)))
+    (save-excursion
+      (forward-char 1)
+      (just-one-space 1))))
 
 (defadvice kill-visual-line (before kill-line-autoreindent activate)
-  (if (member major-mode
-              '(
-                lisp-interaction-mode
-                emacs-lisp-mode
-                scheme-mode
-                lisp-mode
-                c-mode
-                c++-mode
-                latex-mode
-                ))
-      (if (and (eolp) (not (bolp)))
-          (progn (forward-char 1)
-                 (just-one-space 0)
-                 (backward-char 1)))))
+  (when (and (eolp) (not (bolp)))
+    (save-excursion
+      (forward-char 1)
+      (just-one-space 1))))
 
 (defadvice transpose-words (before fix-eob activate)
   (when (let ((str (buffer-substring-no-properties
