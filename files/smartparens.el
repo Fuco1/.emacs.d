bc21ea551ec9be95f5a2cfde6a5a7291267c4690
Matus Goljer <dota.keys@gmail.com>
2015-07-07 20:08:03 +0200

    Delete text pair if followed immediately by space

--- files/smartparens.el
+++ files/smartparens.el
@@ -104,8 +104,9 @@
 (sp-with-modes 'org-mode
   (sp-local-pair "*" "*" :actions '(insert wrap) :unless '(sp-point-after-word-p sp-point-at-bol-p) :wrap "C-*" :skip-match 'sp--org-skip-asterisk)
   (sp-local-pair "_" "_" :unless '(sp-point-after-word-p) :wrap "C-_")
-  (sp-local-pair "/" "/" :unless '(sp-point-after-word-p))
-  (sp-local-pair "~" "~" :unless '(sp-point-after-word-p))
+  (sp-local-pair "/" "/" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
+  (sp-local-pair "~" "~" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
+  (sp-local-pair "=" "=" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
   (sp-local-pair "«" "»"))
 
 (defun sp--org-skip-asterisk (ms mb me)
