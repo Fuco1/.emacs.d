31d3f69636951fece8cadb512a1510518ef08072
Matus Goljer <matus.goljer@gmail.com>
2016-01-31 15:37:56 +0100

    Update custom

--- files/emacs-custom.el
+++ files/emacs-custom.el
@@ -222,7 +222,7 @@
    (quote
     (".cm/" ".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".hi")))
  '(cua-enable-cua-keys nil)
- '(cua-mode t nil (cua-base))
+ '(cua-mode nil nil (cua-base))
  '(cursor-color "#839496")
  '(cursor-type (quote box))
  '(custom-enabled-themes (quote (my-tango-dark)))
@@ -1648,7 +1648,7 @@
  '(rcirc-fill-column (quote frame-width))
  '(rcirc-server-alist (quote (("chat.freenode.org"))))
  '(recentf-auto-cleanup (quote never))
- '(recentf-exclude (quote ("\"elpa/archives\"")))
+ '(recentf-exclude (quote ("\"elpa/archives\"")) t)
  '(recentf-max-saved-items 200)
  '(recentf-save-file "~/.emacs.d/.recentf")
 '(reftex-label-alist
@@ -2018,6 +2018,7 @@
    (sql-port 30239)))))
  '(sql-pop-to-buffer-after-send-region nil)
  '(system-time-locale "C" t)
+ '(tab-always-indent (quote complete))
  '(texmathp-tex-commands (quote (("derivation" env-on))))
  '(text-mode-hook (quote (text-mode-hook-identify)))
  '(tracking-ignored-buffers (quote ("#openttd")))
