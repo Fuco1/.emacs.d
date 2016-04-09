b6404d7fb611c627b7bd199528e777667ae0ca78
Matus Goljer <matus.goljer@gmail.com>
2016-04-09 17:28:07 +0200

    Add comments to advices.el

--- site-lisp/advices.el
+++ site-lisp/advices.el
@@ -1,17 +1,24 @@
 ;; from simple.el
 (defadvice kill-line (before kill-line-autoreindent activate)
+  "Kill excess whitespace when joining lines.
+
+If the next line is joined to the current line, kill the extra indent whitespace in front of the next line."
   (when (and (eolp) (not (bolp)))
     (save-excursion
       (forward-char 1)
       (just-one-space 1))))
 
 (defadvice kill-visual-line (before kill-line-autoreindent activate)
+  "Kill excess whitespace when joining lines.
+
+If the next line is joined to the current line, kill the extra indent whitespace in front of the next line."
   (when (and (eolp) (not (bolp)))
     (save-excursion
       (forward-char 1)
       (just-one-space 1))))
 
 (defadvice transpose-words (before fix-eob activate)
+  "If at eob, transpose the two last words."
   (when (let ((str (buffer-substring-no-properties
                     (point)
                     (save-excursion
@@ -21,6 +28,7 @@
     (backward-word)))
 
 (defadvice subword-transpose (before fix-eob activate)
+  "If at eob, transpose the two last words."
   (when (let ((str (buffer-substring-no-properties
                     (point)
                     (save-excursion
