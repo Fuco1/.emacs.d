5dc63c4ab2f214e3ab617a24cff54a62a982abf4
Matus Goljer <matus.goljer@gmail.com>
2016-05-06 15:10:42 +0200

    Add melpa-stable repository

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
 (use-package-with-elapsed-timer "Initializing packages"
