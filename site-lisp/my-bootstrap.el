ecf4d29c6356574b5d7f7639ddce7b877e63271b
Matus Goljer <matus.goljer@gmail.com>
2017-05-06 14:54:11 +0200

    Rename bootstrap and add my-setup-load-path

--- site-lisp/my-bootstrap.el
+++ site-lisp/my-bootstrap.el
@@ -1,6 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
 (require 'dash)
+(require 'f)
+(require 's)
 
 (defun my-create-cache ()
   "Create .cache directories based on saved custom settings and a
@@ -17,7 +19,7 @@ list of always-create directories."
             (while (setq expr (read (current-buffer)))
               (when (eq (car expr) 'custom-set-variables)
                 (-each (cdr expr)
-                  (-lambda ((_quote (name value)))
+                  (-lambda ((_quote (_name value)))
                     (and (stringp value)
                          (string-match-p "\\.emacs\\.d/\\.cache" value)
                          (push value cache-dirs))))
@@ -34,3 +36,25 @@ list of always-create directories."
           (unless (file-exists-p dir)
             (message "Creating directory: %s" dir)
             (mkdir dir :parents)))))))
+
+(defun my-setup-load-path ()
+  "Add extra entries to `load-path', such as paths to dev
+  versions of packages."
+  (add-to-list 'load-path "/home/matus/dev/c++/ledger/lisp")
+  (add-to-list 'load-path "/home/matus/.emacs.d/dev/legalese")
+  (add-to-list 'load-path "~/.emacs.d/site-lisp/")
+  (add-to-list 'load-path "~/.emacs.d/site-lisp/special/")
+  (mapc (lambda (dir)
+          (add-to-list 'load-path dir)
+          (-when-let
+              (autoloads (--filter
+                          (string-match-p "autoloads\\.el$" it)
+                          (f-files dir)))
+            (mapc 'load autoloads)))
+        (f-directories "~/.emacs.d/vendor"))
+  (mapc (apply-partially 'add-to-list 'load-path) (f-directories "~/.emacs.d/projects"))
+  (mapc (apply-partially 'add-to-list 'load-path) (f-directories "~/.emacs.d/dev/"))
+  (mapc (lambda (dir) (load (concat dir "/" (f-base dir) "-autoloads.el") t t))
+        (f-directories "~/.emacs.d/dev/")))
+
+(provide 'my-bootstrap)
