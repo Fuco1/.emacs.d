4d0da260df15858ddf45011c7658ff2aaa7ed5b6
Matus Goljer <matus.goljer@gmail.com>
2017-05-24 16:39:40 +0200

    Add the org-babel override to init to fix the macro

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (server-start)
 (defconst emacs-start-time (current-time))
+(defmacro org-babel-header-args-safe-fn (safe-list) t)
 
 ;; Emacs gurus don't need no stinking scroll bars & widgets
 (scroll-bar-mode -1)
