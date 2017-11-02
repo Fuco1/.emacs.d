c622b3f7167bc768d4fb1c5800a951421c2d010c
Matus Goljer <matus.goljer@gmail.com>
2017-11-02 12:11:58 +0100

    Open snapshots in js2-mode

--- files/vendor.el
+++ files/vendor.el
@@ -1020,7 +1020,8 @@ use a directory-local variable to specify this per-project."
              t ("-d" "cs_CZ" "-p" "/home/matus/.hunspell/personal.cs") nil utf-8)))))
 
 (use-package js2-mode
-  :mode ("\\.js\\'" . js2-mode)
+  :mode (("\\.js\\'" . js2-mode)
+         ("\\.js\\.snap\\'" . js2-mode))
   :config
   (progn
     (bind-key "C-c C-m"
