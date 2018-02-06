8e564773aeb107a7079af307f9b6e3ce3e2b1b69
Matus Goljer <matus.goljer@gmail.com>
2018-02-06 12:49:28 +0100

    Show submodule info in magit status buffer

--- files/vendor.el
+++ files/vendor.el
@@ -1214,6 +1214,21 @@ called, percentage usage and the command."
          )
   :config
   (progn
+    (magit-add-section-hook 'magit-status-sections-hook
+                            'magit-insert-modules-unpulled-from-upstream
+                            'magit-insert-unpulled-from-upstream)
+    (magit-add-section-hook 'magit-status-sections-hook
+                            'magit-insert-modules-unpulled-from-pushremote
+                            'magit-insert-unpulled-from-upstream)
+    (magit-add-section-hook 'magit-status-sections-hook
+                            'magit-insert-modules-unpushed-to-upstream
+                            'magit-insert-unpulled-from-upstream)
+    (magit-add-section-hook 'magit-status-sections-hook
+                            'magit-insert-modules-unpushed-to-pushremote
+                            'magit-insert-unpulled-from-upstream)
+    (magit-add-section-hook 'magit-status-sections-hook
+                            'magit-insert-submodules
+                            'magit-insert-unpulled-from-upstream)
     (bind-key "<tab>" 'magit-section-toggle magit-mode-map)
     (require 'flyspell)))
 
