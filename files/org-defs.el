2a4f99d6e3e9892a8e57207b4cee34c41dea013c
Matus Goljer <matus.goljer@gmail.com>
2018-04-15 16:36:36 +0200

    Better dwim return in org-mode

--- files/org-defs.el
+++ files/org-defs.el
@@ -877,7 +877,7 @@ properly aligned."
 
 (bind-keys :map org-mode-map
   ("TAB" . smart-tab)
-  ("RET" . scimax/org-return)
+  ("RET" . my-org-return)
   ("<tab>" . smart-tab)
   ("C-e" . my-end-of-code-or-line)
   ("C-a" . my-back-to-indentation-or-beginning)
@@ -1647,60 +1647,82 @@ https://www.pse.cz/udaje-o-trhu/akcie/"
                  year mon day ticker p)))
      data)))
 
-(defun scimax/org-return (&optional ignore)
+(defun my-org-clear-current-line-and-return ()
+  "Clear the current line and call `org-return'."
+  (beginning-of-line)
+  (setf (buffer-substring
+         (line-beginning-position)
+         (line-end-position))
+        "")
+  (org-return))
+
+;; Based on scimax/org-return
+(defun my-org-return (&optional ignore)
   "Add new list item, heading or table row with RET.
 A double return on an empty element deletes it.
 Use a prefix arg to get regular RET. "
   (interactive "P")
-  (if ignore
-      (org-return)
-    (cond
-     ((eq 'line-break (car (org-element-context)))
-      (org-return-indent))
-     ;; Open links like usual
-     ((eq 'link (car (org-element-context)))
-      (org-open-at-point-global))
-     ;; It doesn't make sense to add headings in inline tasks. Thanks Anders
-     ;; Johansson!
-     ((and (fboundp 'org-inlinetask-in-task-p) (org-inlinetask-in-task-p))
-      (org-return))
-     ;; add checkboxes
-     ((org-at-item-checkbox-p)
-      (org-insert-todo-heading nil))
-     ;; lists end with two blank lines, so we need to make sure we are also not
-     ;; at the beginning of a line to avoid a loop where a new entry gets
-     ;; created with only one blank line.
-     ((and (org-in-item-p) (not (bolp)))
-      (let ((context (org-element-context)))
-        (if (or (org-element-property :contents-begin context)
+  (if ignore (org-return)
+    (let* ((context (org-element-context))
+           (syntax-type (car context)))
+      (cond
+       ((eq 'line-break syntax-type)
+        (org-return-indent))
+       ;; Open links like usual
+       ((eq 'link syntax-type)
+        (org-open-at-point-global))
+       ;; It doesn't make sense to add headings in
+       ;; inline tasks. Thanks Anders Johansson!
+       ((and (fboundp 'org-inlinetask-in-task-p)
+             (org-inlinetask-in-task-p))
+        (org-return))
+       ;; add checkboxes
+       ((and (not (bolp))
+             (org-at-item-checkbox-p))
+        (org-insert-todo-heading nil))
+       ;; lists end with two blank lines, so we need
+       ;; to make sure we are also not at the
+       ;; beginning of a line to avoid a loop where
+       ;; a new entry gets created with only one
+       ;; blank line.
+       ((and (not (bolp)) (org-in-item-p))
+        (if (or (org-element-property
+                 :contents-begin context)
                 (and (eq (car context) 'verbatim)
-                     (org-element-property :contents-begin
-                       (org-element-property :parent context))))
-            (org-insert-heading)
-          (beginning-of-line)
-          (setf (buffer-substring
-                 (line-beginning-position) (line-end-position)) "")
-          (org-return))))
-     ((org-at-heading-p)
-      (if (not (string= "" (org-element-property :title (org-element-context))))
-          (progn (org-end-of-meta-data)
-                 (org-insert-heading))
-        (beginning-of-line)
-        (setf (buffer-substring
-               (line-beginning-position) (line-end-position)) "")))
-     ((org-at-table-p)
-      (if (-any?
-           (lambda (x) (not (string= "" x)))
-           (nth
-            (- (org-table-current-dline) 1)
-            (org-table-to-lisp)))
-          (org-return)
-        ;; empty row
-        (beginning-of-line)
-        (setf (buffer-substring
-               (line-beginning-position) (line-end-position)) "")
-        (org-return)))
-     (t
-      (org-return)))))
+                     (org-element-property
+                      :contents-begin
+                       (org-element-property
+                        :parent context))))
+            (org-insert-item)
+          (delete-region (line-beginning-position)
+                         (line-end-position))
+          (org-return)))
+       ((and (not (bolp)) (org-at-heading-p))
+        (if (not
+             (string=
+              ""
+              (org-element-property :title context)))
+            (org-insert-heading-respect-content)
+          (delete-region (line-beginning-position)
+                         (line-end-position))))
+       ((org-at-table-p)
+        (let ((current-line
+               (save-excursion
+                 (save-restriction
+                   (widen)
+                   (narrow-to-region
+                    (line-beginning-position)
+                    (line-end-position))
+                   (car (org-table-to-lisp))))))
+          (if (or (eq current-line 'hline)
+                  (--any? (not (string= "" it))
+                          current-line))
+              (org-return)
+            ;; empty row
+            (delete-region (line-beginning-position)
+                           (line-end-position))
+            (org-return))))
+       (t
+        (org-return))))))
 
 (provide 'org-defs)
