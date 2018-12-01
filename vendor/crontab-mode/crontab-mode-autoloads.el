4c63e45186fa9efec202a60020322b2951bb3649
Matus Goljer <matus.goljer@gmail.com>
2018-12-01 15:22:45 +0100

    Add crontab-mode

---
+++ vendor/crontab-mode/crontab-mode-autoloads.el
@@ -0,0 +1,33 @@
+;;; crontab-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "crontab-mode" "crontab-mode.el" (23554 39164
+;;;;;;  408629 146000))
+;;; Generated autoloads from crontab-mode.el
+
+(autoload 'crontab-mode "crontab-mode" "\
+Major mode for editing crontabs.
+Defines commands for getting and applying crontabs for hosts.
+Sets up command `font-lock-mode'.
+
+\\{crontab-mode-map}
+
+\(fn)" t nil)
+
+(autoload 'crontab-get "crontab-mode" "\
+Get the crontab for the HOST into a buffer.
+
+\(fn HOST)" t nil)
+
+;;;***
+
+(provide 'crontab-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; crontab-mode-autoloads.el ends here
