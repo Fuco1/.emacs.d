737fcfb22aaffc376844d7801e597f115e1af6c2
Matus Goljer <matus.goljer@gmail.com>
2019-01-02 09:36:12 +0100

    Remove markdown-defs.el

--- files/vendor.el
+++ files/vendor.el
@@ -1637,7 +1637,27 @@ If in the test file, visit source."
   :mode ("\\.md$" . gfm-mode)
   :config
   (progn
-    (load-relative "markdown-defs")))
+    (defun my-markdown-init ()
+      (modify-syntax-entry ?\" "$\""))
+    (add-hook 'gfm-mode-hook 'my-markdown-init)
+
+    (defun my-markdown-generate-anchors ()
+      "Add anchors above each header.  If an anchor is present,
+delete it and re-insert new one."
+      (interactive)
+      (let (m)
+        (while (re-search-forward "^\\(#+\\) \\(.*\\)" nil t)
+          (setq m (match-string 2))
+          (beginning-of-line)
+          (previous-line)
+          (if (looking-at "<a")
+              (delete-region (point) (line-end-position))
+            (newline))
+          (insert (concat
+                   "<a name=\""
+                   (replace-regexp-in-string " " "-" (downcase m))
+                   "\" />"))
+          (next-line 2))))))
 
 (use-package message
   :defer t
