3e9c8500d65f4fd00adcc1233e75f83df291f007
Matus Goljer <matus.goljer@gmail.com>
2019-02-11 23:25:49 +0100

    custom(org): remove org-structure-template-alist

--- files/emacs-custom.el
+++ files/emacs-custom.el
@@ -1572,59 +1572,6 @@
      ("plantuml" . fundamental))))
  '(org-src-preserve-indentation t)
  '(org-src-window-setup (quote current-window))
- '(org-structure-template-alist
-   (quote
-    (("s" "#+BEGIN_SRC ?
-
-#+END_SRC" "<src lang=\"?\">
-
-</src>")
-     ("e" "#+BEGIN_EXAMPLE
-?
-#+END_EXAMPLE" "<example>
-?
-</example>")
-     ("q" "#+BEGIN_QUOTE
-?
-#+END_QUOTE" "<quote>
-?
-</quote>")
-     ("v" "#+BEGIN_VERSE
-?
-#+END_VERSE" "<verse>
-?
-</verse>")
-     ("V" "#+BEGIN_VERBATIM
-?
-#+END_VERBATIM" "<verbatim>
-?
-</verbatim>")
-     ("c" "#+BEGIN_CENTER
-?
-#+END_CENTER" "<center>
-?
-</center>")
-     ("l" "#+BEGIN_LaTeX
-?
-#+END_LaTeX" "<literal style=\"latex\">
-?
-</literal>")
-     ("L" "#+LaTeX: " "<literal style=\"latex\">?</literal>")
-     ("h" "#+BEGIN_HTML
-?
-#+END_HTML" "<literal style=\"html\">
-?
-</literal>")
-     ("H" "#+HTML: " "<literal style=\"html\">?</literal>")
-     ("a" "#+BEGIN_ASCII
-?
-#+END_ASCII" "")
-     ("A" "#+ASCII: " "")
-     ("i" "#+INDEX: ?" "#+INDEX: ?")
-     ("I" "#+INCLUDE: %file ?" "<include file=%file markup=\"?\">")
-     ("b" "#+BEGIN: ?
-
-#+END" "<pre>?</pre>"))))
  '(org-stuck-projects (quote ("" nil nil "")))
  '(org-super-agenda-unmatched-order 10)
  '(org-support-shift-select t)
