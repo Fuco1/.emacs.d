844646d4530668317bff64da2aeaad39b730ff8a
Matus Goljer <matus.goljer@gmail.com>
2022-06-02 19:14:07 +0200

    feat(org): org setup on new straight works without a hack

--- init.el
+++ init.el
@@ -45,40 +45,8 @@
   (setq abbrev-file-name "/home/matus/.emacs.d/abbrev_defs")
   (setq diary-file "/home/matus/org/diary"))
 
-(my-with-elapsed-timer "setup org-plus-contrib"
-  (require 'subr-x)
-  (straight-use-package 'git)
-
-  (defun org-git-version ()
-    "The Git version of org-mode.
-Inserted by installing org-mode or when a release is made."
-    (require 'git)
-    (let ((git-repo (expand-file-name
-                     "straight/repos/org/" user-emacs-directory)))
-      (string-trim
-       (git-run "describe"
-                "--match=release\*"
-                "--abbrev=6"
-                "HEAD"))))
-
-  (defun org-release ()
-    "The release version of org-mode.
-Inserted by installing org-mode or when a release is made."
-    (require 'git)
-    (let ((git-repo (expand-file-name
-                     "straight/repos/org/" user-emacs-directory)))
-      (string-trim
-       (string-remove-prefix
-        "release_"
-        (git-run "describe"
-                 "--match=release\*"
-                 "--abbrev=0"
-                 "HEAD")))))
-
-  (provide 'org-version)
-  (straight-use-package 'org-plus-contrib)
-  (straight-use-package 'org)
-  )
+(straight-use-package 'org-contrib)
+(straight-use-package 'org)
 
 (straight-use-package 'use-package)
 (setq use-package-verbose t)
